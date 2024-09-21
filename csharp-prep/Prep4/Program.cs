bb46d9e5c3cd967965f311426fc9e5d35572714e
Krelmoon <114534346+Krelmoon@users.noreply.github.com>
2024-09-21 19:49:05 -0400

    Wrote Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = (1);
+        do
+        {
+            Console.Write("Enter Number: ");
+            string numberInput = Console.ReadLine();
+            number = int.Parse(numberInput);
+            numbers.Add(number);
+        } while (number != 0);
+        {
+            int sum = numbers.AsQueryable().Sum();
+            Console.WriteLine($"The sum is: {sum}");
+            double average = Queryable.Average(numbers.AsQueryable()); 
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {numbers.Max(z => z)}");
+        }
+        
     }
 }
