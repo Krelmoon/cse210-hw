872131fbe924821a0fba08ea18d745689ed6d5b4
Krelmoon <114534346+Krelmoon@users.noreply.github.com>
2024-09-21 18:59:38 -0400

    Wrote Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Guess the magic number");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        int guess = (0);
+        do
+        {
+        Console.Write("What is your guess ? ");
+        string guessInput = Console.ReadLine();
+        guess = int.Parse(guessInput);
+        if (guess == number)
+        {
+            Console.WriteLine("you guessed correctly!");
+        }
+        else if (guess > number)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("Higher");       
+        }
+        } while (guess != number);
+        {
+            Console.WriteLine("Thank you for playing the game!");
+        }
     }
 }
