76f503ca83c9abe8436af5bda1ed52824d79e8e0
Krelmoon <114534346+Krelmoon@users.noreply.github.com>
2024-09-21 20:45:30 -0400

    Wrote Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,45 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+        return number;
+    }
+
+    static double SquareNumber(int number)
+    {
+        double square = Math.Pow(number, 2);
+        return square;
+    }
+
+    static void DisplayResult(string userName,double square)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {square}.");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int number = PromptUserNumber();
+        double square = SquareNumber(number);
+        DisplayResult(userName,square);
     }
+
+
 }
